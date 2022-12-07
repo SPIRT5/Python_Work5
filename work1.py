4c66b5f3d86f29499d4a06b8bbd6e9ed233d1a91
SPIRT5 <108623559+SPIRT5@users.noreply.github.com>
2022-12-07 18:43:22 +0300

    Add work1
    Add work1

---
+++ work1.py
@@ -0,0 +1,58 @@
+# Создайте программу для игры с конфетами человек против компьютера.
+# Условие задачи: На столе лежит 150 конфет. Играют игрок против компьютера. 
+# Первый ход определяется жеребьёвкой. За один ход можно забрать не более чем 28 конфет.
+# Все конфеты оппонента достаются сделавшему последний ход. Подумайте как наделить бота ""интеллектом""
+
+import random
+
+#Функция для хода игрока
+def player_input (player_name):
+    k = int (input (f'{player_name}, введите количество конфет, которое возьмете от 1 до 28: '))
+    while k < 1 or k > 28:
+        k = int (input (f'{player_name}, введите корректное количество конфет: '))
+    return k
+
+#Функция для хода робота
+def bot_input (konfet):
+    k = random.randrange (1, 29)
+    if ((konfet - k) < 57) :
+        k = konfet - 57
+    if (konfet <= 56 and konfet > 29) :
+        k = konfet - 29
+    while (konfet - k) <= 28 and konfet > 29:
+        k = random.randrange (1, 29)
+    return k
+
+name1 = input ('Введите имя первого игрока: ')
+name2 = input ('Введите имя второго игрока: ')
+
+#Жеребьевка игроков
+igrok = random.randrange (1, 3)
+
+if igrok == 1 :
+    print (f'Первый ход делает: {name1}')
+else :
+    print (f'Первый ход делает: {name2}')
+
+konfet = 150
+count1 = 0
+count2 = 0
+
+while konfet > 28:
+    if igrok == 1:
+        hod = player_input (name1)
+        count1 += hod
+        konfet -= hod
+        igrok = 2
+        print(f'Ходил {name1}, он взял {hod}, теперь у него {count1}. Осталось на столе {konfet} конфет.')
+    else:
+        hod = bot_input (konfet)
+        count2 += hod
+        konfet -= hod
+        igrok = 1
+        print(f'Ходил {name2}, он взял {hod}, теперь у него {count2}. Осталось на столе {konfet} конфет.')
+
+if igrok == 1 :
+    print(f'Выиграл {name1}')
+else:
+    print(f'Выиграл {name2}')
